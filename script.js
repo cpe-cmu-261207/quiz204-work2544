1905d5ba2dc2b39536137e5e2f2f695c4ac35bd8
latthaphol laohapiboonrattana <work21_123@hotmail.com>
2021-07-29 14:19:37 +0700

    zzz

--- script.js
+++ script.js
@@ -5,22 +5,36 @@ const length = document.getElementById('length')
 const color = document.querySelector('#color')
 const mytext=document.querySelector('#text')
 // define more constants and variables here
+const mysearch=document.querySelector('#search')
+const myreset=document.querySelector('#reset')
+
 var check=true;
+let mymytext=mytext.innerHTML.split(" ")
+let mymymytext=[]
+
 btn_toggle.onclick = () => {
   // your code here
   var myauthor=author;
   if(check){
-  myauthor.innerHTML=author[0]
   btn_toggle.innerHTML='Display Author'
   check=false;
   }
   else
   {
-    myauthor.innerHTML=author[1]
     btn_toggle.innerHTML='Display Calculation'
     check=true;
   }
 }
 
-
 // more codes for Search and Reset buttons here
+mysearch.onclick=()=>{
+for (let index = 0; index < mymytext.length; index++) {
+  if (if(mymytext[index])==length.value) {
+    mymytext
+  } 
+  
+}
+}
+myreset.onclick=()=>{
+  
+}
